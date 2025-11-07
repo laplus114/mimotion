8f39689385eca19643ab7e6f7796ec27a10c4fd5
laplus114 <124484047+laplus114@users.noreply.github.com>
2025-11-07 23:10:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ def login(user, password, fake_ip):
     if is_phone  and ("+86" in user):
         user = user
     else:
-        user = "+86" + user
+        user = user
     headers = {
         "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
         "user-agent": "MiFit6.14.0 (M2007J1SC; Android 12; Density/2.75)",
